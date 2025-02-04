3c8dbdaaa8607083829b8523560c023c06481826
Hakua <azureshockwave@gmail.com>
2025-02-04 00:09:15 -0500

    update yml

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,16 @@
-import baseConfig from './next.config.base.js'
 import type { NextConfig } from 'next'
 
-// Debug log during build
-console.log('Next.js Build Environment:', {
-  hasYoutubeKey: !!process.env.NEXT_PUBLIC_YOUTUBE_API_KEY,
-  hasAdminHash: !!process.env.NEXT_PUBLIC_ADMIN_PASSWORD_HASH,
-  publicVars: Object.keys(process.env).filter(key => key.startsWith('NEXT_PUBLIC_')),
-});
-
-const config: NextConfig = {
-  ...baseConfig,
-  // Any additional config you need
+const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 }
 
-export default config
+export default nextConfig
